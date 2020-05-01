0f0254d78f242844c4b8246049e6050925f9e59d
Sam <eldreda123@gmail.com>
2020-05-01 17:06:03 +0700

    completed

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,73 @@
 //
 
 import UIKit
-
+import AVFoundation
+//
+//class ViewController: UIViewController {
+//
+//    var player: AVAudioPlayer?
+//
+//    func playSound() {
+//
+//        guard let url = Bundle.main.url(forResource: "C", withExtension: "wav") else { return }
+//
+//        do {
+//            //play the sound
+//            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+//            try AVAudioSession.sharedInstance().setActive(true)
+//
+//            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+//
+//
+//            guard let player = player else { return }
+//
+//            player.play()
+//
+//        } catch let error {
+//            print(error.localizedDescription)//catch error
+//        }
+//
+//
+//    }
+//
+//    @IBAction func keyPressed(_ sender: UIButton) {
+//        playSound()
+//    }
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//    }
+//
+//
+//
+//
+//}
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        playSound(note:sender.currentTitle!)
+        sender.alpha = 0.5
+        
+        let seconds = 0.2
+        DispatchQueue.main.asyncAfter(deadline: .now() + seconds) {
+            // Put your code which should be executed with a delay here
+            sender.alpha = 1
+        }
+        
+    }
     
-
+    func playSound(note: String) {
+        let url = Bundle.main.url(forResource: note, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
 }
+
 
